9204b74769b278e7be531eae281e3c946eef3d54
Method Mkoma <gershommethod02@gmail.com>
2024-07-04 18:28:53 +0300

    task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+''' type-annotated function '''
+
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    n: float = 0.0
+    for i in input_list:
+        n += i
+    return n
