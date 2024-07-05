6b9ccffaf4776baca4177b65c4ce0a879df7ee2e
Method Mkoma <gershommethod02@gmail.com>
2024-07-06 01:38:22 +0300

    task 9

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" function’s parameters and return values """
+
+
+from typing import List, Tuple, Sequence
+
+
+def element_length(lst: List[Sequence]) -> List[Tuple[Sequence, int]]:
+    ''' Function itself '''
+    return [(i, len(i)) for i in lst]
