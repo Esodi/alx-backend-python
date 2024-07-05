28e676b44a20643ed437fe579ea963603c6b894d
Method Mkoma <gershommethod02@gmail.com>
2024-07-05 19:42:17 +0300

    task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" type-annotated function sum_mixed_list which takes a list mxd_lst """
+
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    ''' fuction itself'''
+    s: float = 0.0
+    for i in mxd_lst:
+        s += i
+    return s
