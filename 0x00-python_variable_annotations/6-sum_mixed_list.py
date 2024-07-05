fa31b6c042b43790be38d96abdc6161aed41ad49
Method Mkoma <gershommethod02@gmail.com>
2024-07-05 19:53:11 +0300

    task 6

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -7,4 +7,5 @@ from typing import List, Union
 
 def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
     ''' fuction itself'''
-    return float(sum(mxd_lst))
+    tot: float = sum(mxd_lst)
+    return tot
