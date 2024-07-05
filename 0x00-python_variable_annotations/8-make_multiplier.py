875f30c85bd5ed8d0f864fb9362013f07db5cd6a
Method Mkoma <gershommethod02@gmail.com>
2024-07-06 01:29:14 +0300

    task 8

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+''' type-annotated function make_multiplier '''
+
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    ''' function itself '''
+    def func(multiplier: float) -> float:
+        return multiplier ** 2
+    return func
