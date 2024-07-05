98dd2a92113c2fe9fbb77d7f2251827b0e876f6e
Method Mkoma <gershommethod02@gmail.com>
2024-07-05 21:27:44 +0300

    task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -2,9 +2,9 @@
 """ function to_kv that takes a string k and an int OR float v as arguments and returns a tuple. The first element of the tuple is the string k """
 
 
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: float) -> tuple:
+def to_kv(k: str, v: Union(int, float)) -> Tuple[str, float]:
     ''' function itself '''
-    return (k, v**2)
+    return (k, float(v**2))
