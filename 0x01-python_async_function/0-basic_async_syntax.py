fa28a042d106d1b680d6ae21e33f158aa831efa2
Method Mkoma <gershommethod02@gmail.com>
2024-07-08 21:36:59 +0300

    task 0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+''' asyncio and random using '''
+
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    rand: float = random.uniform(0, max_delay)
+    await asyncio.sleep(rand)
+    return rand
