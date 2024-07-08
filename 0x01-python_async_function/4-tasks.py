ef9dc3fac463a5ee9cb3655aab989bcbcb0ee5a7
Method Mkoma <gershommethod02@gmail.com>
2024-07-08 23:26:33 +0300

    task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -9,6 +9,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 task_wait_random = __import__('3-tasks').task_wait_random
 
+
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     ''' function itself '''
     tasks = [task_wait_random(max_delay) for _ in range(n)]
